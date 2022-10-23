7aa7b31ad70f08e38084fe5f935e3b667ffd84b4
Sergio Filho <filho.sf14@gmail.com>
2022-10-23 16:16:56 -0300

    ...

--- Trabalho1/project/parte1.c
+++ Trabalho1/project/parte1.c
@@ -14,22 +14,12 @@ int max (int arr[], int len){
     return max_value;
 }
 
-bool equal (int arr1[], int arr2[], int n) {
-    for (int i = 0; i < n; i++){
-        if (arr1[i] != arr2[i])
-            return false;
-    }
-    return true;
-}
-
 // ------------------------------------------------------------------------------------
 
 #define N_TASKS 3
+#define N_COLS 3
 #define N_LINES N_TASKS+1
 #define EXECUTION_TIME 50
-
-char menu_option[4] = {'0','0','0','0'};
-char system_matrix[N_TASKS][3];
 
 struct task {
     int p;
@@ -40,6 +30,7 @@ struct task {
 struct system {
     struct task tasks[N_TASKS];
     char execution_time_table[N_TASKS][EXECUTION_TIME];
+    int tasks_queue[N_TASKS];
 };
 
 struct system system;
@@ -72,35 +63,54 @@ bool scalability_test () {
 void print_system_matrix (void) {
     printf("--------\n");
     printf("\tP\tC\tD\n");
-    for (int j=0; j<N_LINES; j++) {
+    for (int j = 0; j < N_LINES; j++) {
         printf("T%d\t%d\t%d\t%d\n", j+1, system.tasks[j].p, system.tasks[j].c, system.tasks[j].d);
     }
     printf("---------\n\n");
 }
 
-void RM (void) {
+void rate_monotonic (void) {
     printf("Rate Monotonic\n");
+
+    int max_value = arr[0];
+
+    for (int i = 1; i < len; i++) {
+        if (arr[i] > max_value){
+            max_value = arr[i];
+        }
+    }
+    return max_value;
+    
+
+    for (int t = 0; t < EXECUTION_TIME; t++){
+        for (int i = 0; i < N_TASKS; i++){
+
+        }
+    }
 }
 
-void DM (void) {
+void deadline_monotonic (void) {
     printf("Deadline Monotonic\n");
 }
 
-void EDF (void) {
+void earliest_deadline_first (void) {
     printf("->Earliest Deadline First<-\n");
     printf("------. Escala de Tempo .------\n");
     
     for (int t = 0; t < EXECUTION_TIME; t++) {
         for (int i = 0; i < N_TASKS; i++) {
             
-            system.execution_time_table[i][t] = "O";
+            //system.execution_time_table[i][t] = "O";
         }
     }
 }
 
-int read_file (char system_file[]) {
+int read_file (char system_file[]) {    
     FILE *file_ptr = fopen(system_file, "r");
-    rewind(file_ptr);
+    if (file_ptr == NULL) {
+        printf("no such file.\n");
+        return 0;
+    }
     
     /* 
     Assuming that system.txt has content in below format:
@@ -109,19 +119,20 @@ int read_file (char system_file[]) {
         5       2       5
         20      4       20
     */
-    if (file_ptr == NULL) {
-        printf("no such file.\n");
-        return 0;
-    }
-    
+
     for (int i = 0; i < N_LINES; i++){
-        fscanf(file_ptr, "%d %d %d ", &P[i], &C[i], &D[i]);
+        fscanf(file_ptr, "%d %d %d ", &system.tasks[i].p, &system.tasks[i].c, &system.tasks[i].d);
     }
 
-    P[0] = 0;
-    C[0] = 0;
-    D[0] = 0;
-
+    /*
+    char save[N_COLS];
+    for (int i = 0; fgets(save, N_COLS, file_ptr) != NULL; i++){
+        for (int j = 0; j < N_COLS; j++){
+            system.tasks[i].p = save[0];
+            system.tasks[i].c = save[1];
+            system.tasks[i].d = save[2];
+        }
+    }*/
     return 0;
 }
 
@@ -134,6 +145,7 @@ int main(void) {
     
     print_system_matrix();
 
+    char menu_option[4] = {'0','0','0','0'};
     while (strcmp(menu_option, "exit")) {
         printf("----------------------------------------------\n");
         printf("Escolha o Algoritmo de Escalonamento ou <exit> para sair:\n");
@@ -145,13 +157,13 @@ int main(void) {
         scanf("%s", menu_option);
 
         if (!strcmp(menu_option, "RM")) {
-            RM();
+            rate_monotonic();
         } else
         if (!strcmp(menu_option, "DM")) {
-            DM();
+            deadline_monotonic();
         } else
         if (!strcmp(menu_option, "EDF")){
-            EDF();
+            earliest_deadline_first();
         } else
         if (!strcmp(menu_option, "exit")){
             printf("Saindo...\n");
